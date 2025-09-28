e0ea7e8a1509428328c323a15a8551d71d5b828e
Rakshit024 <patelrakshit024@gmail.com>
2025-09-29 04:56:04 +0530

    2nd

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,6 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   await mongoose
     .connect(process.env.CONNECTION_STRING)
-    .then(() => console.log("MongoDB connected"));
+    .then(() => console.log("MongoDB connected...."));
 };
 module.exports = connectDB;
